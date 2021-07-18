186ce994513a17038b817e8ed1779e673a3c8ad8
Mark Jenkins <mark@markjenkins.ca>
2021-07-18 13:35:55 -0500

    minimal viable product

---
+++ README.md
@@ -0,0 +1,42 @@
+This is a work in progress re-implementation of the Manitoba Immunization Card verification app as a GNU Bash script. Relies on grep, egrep, sed, wget, and zbarcam (zbar-tools) being available.
+
+These are not the best tools for the job. Particularly for parsing JSON and working with the authentication layer.
+
+The official published Android app is 50MB. My use of a bash script, measured in kilobytes is just a goofy code golf exercise to show it can be done this way, and with an output device as simple as a teletype or speaker. It will be the basis of a demo in the form of a immunization verification "machine".
+
+This is product of reverse engineering and does not rely on an officially documented API. As such, incompatible changes are possible without notice.
+
+For now, I have skipped any understanding of the authentication API. To use this app, log into the portal (https://immunizationcard.manitoba.ca/) with your web browser. Enable developer tools and extract a Bearer token from the authorization header. Put the bearer token on a single line in bearertoken.txt . (a trailing newline is fine).
+
+Don't include the header name "authorization:" and the "Bearer" part in bearertoken.txt . You can see in the source code these are already included in our requests. Just include the token itself.
+
+Unfortunately, the bearer tokens only last an hour or two. There also appears to be a refresh token that lasts for 24 hours, but there's no refresh logic to be seen here.... yet.
+
+As such, this is not a practical implementation that you would want to use at a restaurant seating multiple households indoors or any other venue required to check immunization status. Use at your own risk.
+
+Some additional information about the authentication API is in my mailing list post
+https://groups.google.com/a/skullspace.ca/g/announce/c/BTF30UKo0I4/m/wOKLxSuqAAAJ
+
+This application reads and processes external inputs: QR codes supplied by other people and JSON data in the government response. This has security implications, see
+https://xkcd.com/327/
+
+This implementation has not received a security audit. The choice of GNU Bash poses significant risks, a security bug could result in arbitrary shell commands being run.
+
+And additional reason to say again, use at your own risk.
+
+--------------------
+
+Copyright 2021 Mark Jenkins
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ verificationpipeline.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 Mark Jenkins
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+CARDSERVER="https://immunizationcard.manitoba.ca"
+VERIFICATION_URL="$CARDSERVER/api/verification"
+
+# QR-Code: is the prefix we're expecting in the output of zbarcam
+CARD_PREFIX_W_QR_PREFIX="QR-Code:https://immunizationcard.manitoba.ca/?ID="
+
+# set this to have zbarcam use a different /dev/video* device than the default
+# e.g. VIDEODEVICE="/dev/video1"
+VIDEODEVICE=""
+
+if ! test -f bearertoken.txt; then \
+    echo "bearertoken.txt file does not exist" 1>&2
+    exit 1
+fi
+bearertoken=$(<bearertoken.txt)
+
+function processuuid {
+    # ask the government if a UUID ($1 first argument) is a valid
+    # immunization card
+    responsejson=$(wget -q -O - \
+			--header "authorization: Bearer $bearertoken" \
+			"$VERIFICATION_URL/$1" 2>/dev/null)
+
+    # check if we got a response with the JSON payload containing a
+    # isValid field, if that's not there something is very wrong, so exit
+    if ! grep 'isValid'<<<"$responsejson">/dev/null; then \
+	echo "immunization server response was not what we expected; exiting" \
+	     1>&2
+	echo "possibly the bearer token is expired" 1>&2
+	exit 1
+    fi
+
+    # if the response contains "isValid": true, with whatever use of whitespace
+    if grep '"isValid"\w*:\w*true'<<<$responsejson >/dev/null; then \
+	# exact firstName and lastName fields expected in a valid response
+	# (we should probably check they're there)
+	#
+	# this isn't even close to a proper JSON parser
+	# for example, considering the firstName to end with double quote '"'
+	# doesn't account for the fact that json has escape sequences for
+	# such things!
+	firstName=$(sed -e 's@^.*"firstName"\w*:\w*"\([^"]*\)".*$@\1@'\
+			<<<"$responsejson")
+	lastName=$(sed -e 's@^.*"lastName"\w*:\w*"\([^"]*\)".*$@\1@'\
+		       <<<"$responsejson")
+
+	echo $firstName $lastName Verification successful;
+    # otherwise, consider verificaiton to have failed
+    else \
+	echo Verification failed
+    fi
+}
+
+function processimmunizationcards {
+    # for each potential immunization card on standard in
+    while read immunizationcard; do \
+	# check for the zbarcam QR code prefix and the government URL prefix
+	if grep "^$CARD_PREFIX_W_QR_PREFIX"<<<"$immunizationcard" >/dev/null;
+	then \
+	    # extract the UUID from the read line
+	    uuid=$(sed -e "s@^$CARD_PREFIX_W_QR_PREFIX@@"\
+		       <<<"$immunizationcard")
+	    # validate the format of the uuid
+	    # if we wanted to get fancier we would verify a type 4 (random),
+	    # sub-variant 1 (standard) UUID
+	    if egrep "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"<<<"$uuid" >/dev/null;
+	    then \
+		# delegate processing an exacted immunization card UUID
+		# to another function
+		processuuid "$uuid"
+	    fi
+	fi
+    done;
+}
+
+# our main pipeline, zbarcam will output a line for every barcode/QR code
+# that it sees
+#
+# we delegate handling each of those lines to a function
+zbarcam --nodisplay $VIDEODEVICE | processimmunizationcards
